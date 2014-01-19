23241ffba2180f06a87aa62d9e5d350cfda41b00
Robert Myers <rob@wheelinnovationz.com>
2014-01-19 10:12:15 -0600

    Add copyright notice

--- src/com/robandjen/chargemonitor/BootReceiver.java
+++ src/com/robandjen/chargemonitor/BootReceiver.java
@@ -14,6 +14,7 @@
 //    along with ChargeMonitor.  If not, see <http://www.gnu.org/licenses/>.
 //    	
 
+/* Copyright 2014, Robert Myers */
 package com.robandjen.chargemonitor;
 
 import android.content.BroadcastReceiver;
--- src/com/robandjen/chargemonitor/ChargeService.java
+++ src/com/robandjen/chargemonitor/ChargeService.java
@@ -13,7 +13,7 @@
 //    You should have received a copy of the GNU General Public License
 //    along with ChargeMonitor.  If not, see <http://www.gnu.org/licenses/>.
 //    	
-
+/* Copyright 2014, Robert Myers */
 package com.robandjen.chargemonitor;
 
 import android.app.Notification;
