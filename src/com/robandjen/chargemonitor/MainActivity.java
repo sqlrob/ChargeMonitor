46d17794a9199915e8a1492b430c99ec636d8582
Robert Myers <rob@wheelinnovationz.com>
2014-01-20 11:09:48 -0600

    Remove starting notification

--- src/com/robandjen/chargemonitor/MainActivity.java
+++ src/com/robandjen/chargemonitor/MainActivity.java
@@ -17,8 +17,6 @@
 package com.robandjen.chargemonitor;
 
 import android.app.Activity;
-import android.app.Notification;
-import android.app.NotificationManager;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.os.BatteryManager;
@@ -34,15 +32,6 @@ public class MainActivity extends Activity {
 		if (batteryintent.getIntExtra(BatteryManager.EXTRA_PLUGGED, 0) != 0) {
 			startService(new Intent(getApplicationContext(),ChargeService.class));
 		}
-		
-		Notification.Builder builder = new Notification.Builder(this);
-		builder.setSmallIcon(android.R.drawable.ic_lock_idle_low_battery)
-			.setContentTitle(getString(R.string.app_name))
-			.setContentText(getString(R.string.enabled))
-			.setAutoCancel(true);
-		
-		NotificationManager nm = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-		nm.notify(NotificationIDs.StartMessage,builder.build());
 		
 		finish();
 	}
--- src/com/robandjen/chargemonitor/NotificationIDs.java
+++ src/com/robandjen/chargemonitor/NotificationIDs.java
@@ -19,5 +19,4 @@ package com.robandjen.chargemonitor;
 //List of ids used for notification messages
 public class NotificationIDs {
 	final static int WarningMessage = 1;
-	final static int StartMessage = 2;
 }
